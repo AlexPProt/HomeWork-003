c39c5d3f89a0a8412ab0fd3181dc18c9ecd41f42
AlexProt <app78@ya.ru>
2023-04-13 22:52:30 +0300

    Добавил возможность выбора разряда

--- Program.cs
+++ Program.cs
@@ -18,28 +18,57 @@
 //_________________________________________Задание  2.2 _________________________________
 //Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 
-int BackThree (int inputN)
+// int BackThree (int inputN)
+//     {
+//        int del=100;
+//        if (inputN/del!=0)  // проверяем минимальную разрядность введенного числа
+//             {   
+//                 int razr = 2;
+//                 while (inputN/Convert.ToInt32(Math.Pow(10,(razr)))!=0)  // определяем разрядность числа
+//                     {
+//                         razr++;
+//                     }
+//                 return Convert.ToInt32(inputN % Convert.ToInt32(Math.Pow(10,(razr-2)))/Convert.ToInt32(Math.Pow(10,(razr-3)))); 
+//                 // выделяем нужный разряд
+//             }
+//         else return -1;     // возвращяем -1 при отсутствии нужной разрядности
+//     }
+// Console.Write ("Введите обрабатываемое  число - " );
+// int num=Convert.ToInt32(Console.ReadLine());
+// num = BackThree (num);
+// if (num!=-1) Console.WriteLine ($"Результат обработки - {num}");
+// else Console.WriteLine($"Число не удовлетворяет параметрам обработки");
+
+
+
+
+//_________________________________________Задание  2.2 _________________________________
+//Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// Ввожим в функцию доп параметр для вывода нужного разряд
+
+int BackThree (int inputN, int numPP)
     {
-       int del=100;
+       int del=Convert.ToInt32(Math.Pow(10,(numPP-1)));
+
        if (inputN/del!=0)  // проверяем минимальную разрядность введенного числа
             {   
-                int razr = 2;
+                int razr = numPP;
                 while (inputN/Convert.ToInt32(Math.Pow(10,(razr)))!=0)  // определяем разрядность числа
                     {
                         razr++;
                     }
-                return Convert.ToInt32(inputN % Convert.ToInt32(Math.Pow(10,(razr-2)))/Convert.ToInt32(Math.Pow(10,(razr-3)))); 
+                return Convert.ToInt32(inputN % Convert.ToInt32(Math.Pow(10,(razr-(numPP-1))))/Convert.ToInt32(Math.Pow(10,(razr-numPP)))); 
                 // выделяем нужный разряд
             }
         else return -1;     // возвращяем -1 при отсутствии нужной разрядности
     }
 Console.Write ("Введите обрабатываемое  число - " );
 int num=Convert.ToInt32(Console.ReadLine());
-num = BackThree (num);
-if (num!=-1) Console.WriteLine ($"Результат обработки - {num}");
+Console.Write ("Введите номер выводимого разряда  число - " );
+int numR=Convert.ToInt32(Console.ReadLine());
+num = BackThree (num,numR);
+if (num!=-1) Console.WriteLine ($"Обработка вывода разряда {numR}  осуществлена- {num}");
 else Console.WriteLine($"Число не удовлетворяет параметрам обработки");
-
-
 
 //_________________________________________Задание  2.3 _________________________________
 // Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
